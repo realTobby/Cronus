27701642bd0face4b77782ecd85f0f7205d3b681
Tobias Kattanek <tkattanek00@gmail.com>
2021-09-07 11:19:59 +0200

    Editor
    Maybe its smart to start with the actual editor. Created a canvas element and moveable elements.

--- Projects/Cronus/Cronus/MainWindow.xaml.cs
+++ Projects/Cronus/Cronus/MainWindow.xaml.cs
@@ -1,4 +1,12 @@
-﻿namespace Cronus
+﻿using Cronus.DraggableControls;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Cronus
 {
     /// <summary>
     /// Interaktionslogik für MainWindow.xaml
@@ -7,10 +15,12 @@
     {
         private ViewModels.ViewModel originalViewModel;
 
+        Random rnd;
+
         public MainWindow()
         {
             originalViewModel = new ViewModels.ViewModel();
-
+            rnd = new Random();
             InitializeComponent();
 #if DEBUG
             originalViewModel.VersionTitle = "Cronus v0.0 [DEV]";
@@ -20,5 +30,38 @@
 
         }
 
+        void Rectangle_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Source is Shape shape)
+            {
+                if (e.LeftButton == MouseButtonState.Pressed)
+                {
+                    Point p = e.GetPosition(canvas);
+                    Canvas.SetLeft(shape, p.X - shape.ActualWidth / 2);
+                    Canvas.SetTop(shape, p.Y - shape.ActualHeight / 2);
+                    Canvas.SetZIndex(shape, 10);
+                    shape.CaptureMouse();
+                }
+                else
+                {
+                    shape.ReleaseMouseCapture();
+                    Canvas.SetZIndex(shape, -10);
+                }
+            }
+        }
+
+        private void btn_add_element_Click(object sender, RoutedEventArgs e)
+        {
+
+            Rectangle newRectangle = new Rectangle();
+            //Width = '50' Height = '50' Fill = 'LightPink' Canvas.Left = '350' Canvas.Top = '175'
+            newRectangle.Width = 50;
+            newRectangle.Height = 50;
+            newRectangle.Fill = new SolidColorBrush(Color.FromRgb((byte)rnd.Next(0,255), (byte)rnd.Next(0, 255), (byte)rnd.Next(0, 255)));
+            Canvas.SetLeft(newRectangle, 350);
+            Canvas.SetTop(newRectangle, 175);
+            newRectangle.MouseMove += Rectangle_MouseMove;
+            canvas.Children.Add(newRectangle);
+        }
     }
 }
